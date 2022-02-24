23f67a5d62acd4ae01ccff58c2bc0153e3eb64ec
coder-jinyeong <78059103+coder-jinyeong@users.noreply.github.com>
2022-02-24 19:01:33 +0900

    22-02-24

--- components/Bmi.js
+++ components/Bmi.js
@@ -1,10 +1,33 @@
 import React, { useState } from 'react' 
 import Layout from 'containers/Layout'
+import axios from 'axios';
+import { useLinkClickHandler } from 'react-router-dom';
 
 export default function Bmi (){
+/*
     const [username, setUsername] = useState("");
     const [height, setHeight] = useState(0.0);
     const [weight, setWeight] = useState(0.0);
+*/
+    const [inputs, setInputs] = useState({})
+    const { name, weight, height } = inputs; // Object  Destructuring
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        const {value, name } = e.target;
+        setInputs({ ...inputs, [name]: value})
+    }
+    const handleClick = (e) => {
+        e.preventDefault()
+        const bmiRequest = {name,weight,height}
+        alert(` 사용자이름: ${JSON.stringify(bmiRequest)}`)
+        /*
+        axios.get(`http://localhost:8080/member/bmi/김길동/180.5/80.5`)
+            .then((res)=>{
+                alert(`답장이 도착했습니다 [내용] ${JSON.stringify(res.data)}`)
+            })*/
+    }
+    /*
     const result = () => {
 
         let username = document.getElementById('username').value
@@ -17,18 +40,37 @@ export default function Bmi (){
         setHeight(height)
         setWeight(weight)
     }
+    */
 
-    return <Layout><h1>Bmi폼</h1>
+    return (<Layout>
+    <form>
+    <h1>Bmi폼</h1>
     <div>
+
+    <label><b>Username</b></label>
+    <input type="text" name = "name" onChange={handleChange} /><br />
+
+    <label htmlFor=""><b>height</b></label>
+    <input type="text" name = "height" onChange={handleChange} /><br />
+
+    <label htmlFor=""><b>weight</b></label>
+    <input type="text" name = "weight" onChange={handleChange} /><br />
+    
+    <button onClick={handleClick}>BMI 체크</button>
+    
+    </div> 
+    </form>
+    </Layout>)
+}
+ 
+/*
     <label><b>Username</b></label>
     <input id = "username" type="" /><br />
     <label htmlFor=""><b>height</b></label>
     <input id = "height" type="" /><br />
     <label htmlFor=""><b>weight</b></label>
     <input id = "weight" type="" /><br />
+    
     <button onClick={() => {result()}}>제출하기</button>
     <div>이름 : {username} , 키 : {height} , 몸무게 : {weight}</div>
-    </div> 
-    </Layout>
-}
- 
+*/
